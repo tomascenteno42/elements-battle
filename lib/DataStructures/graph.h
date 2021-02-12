20428c0059f9877bf99008d4b9d87c0f45677bcc
Facundo <you@example.comfacuayerza@gmail.com>
2021-02-12 20:51:54 -0300

    Added Graph Structure

---
+++ lib/DataStructures/graph.h
@@ -0,0 +1,135 @@
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "../../src/main.h"
+
+using namespace std;
+
+template <class T>
+class GraphData {
+public:
+    T data;
+    int cost;
+    vector<int> adjList;
+};
+
+template <class T>
+class Graph
+{
+private:
+    int vertices;
+    GenericList<GraphData<T>*> *head;      // GenericList<GraphData>
+    GenericNode<GraphData<T>*> *current;   // GenericNode<GraphData>
+
+public:
+    // CONSTRUCTOR
+    Graph(int v);
+    
+    // Fills index node
+    void FillNode(T data, int cost, int index);
+
+    // Links two nodes
+    void addEdge(int from, int to); // A -> B y B -> A
+
+    // Returns number of vertices
+    int getVertices();
+
+    // Gets data from node in pos
+    GraphData<T>* getData(int pos);
+
+    void printGraph();    // Test Purposes
+
+    // DESTRUCTOR
+    ~Graph();
+};
+
+// IMPLEMENTACION
+
+template <class T>
+Graph<T>::Graph(int v) 
+{
+    vertices = v;
+    head = new GenericList<GraphData<T>*>;
+    current = nullptr;
+
+    for (size_t i = 0; i < vertices; i++){
+        GraphData<T> *nodeData = new GraphData<T>();
+
+        if (head->isEmpty() == 0){
+            head->add(nodeData, 1);
+        }else head->add(nodeData, head->getQuantity() + 1);
+    }
+}
+
+template <class T> 
+void Graph<T>::FillNode(T data, int cost, int index)
+{
+    if (index < vertices && index > 0 && head->get(index) != nullptr){
+        GraphData<T> *graphdata = head->get(index);
+        graphdata->cost = cost;
+        graphdata->data = data;
+    } cout << "FILL NODE ERROR" << endl;
+}
+
+template <class T>
+void Graph<T>::addEdge(int A, int B)
+{
+    if (A < vertices && A > 0 && B < vertices && B > 0 && A != B && head->get(A) != nullptr && head->get(A) != nullptr){
+        GraphData<T> *graphdata; 
+    
+        graphdata = head->get(A);
+        graphdata->adjList.push_back(B);
+
+        graphdata = head->get(B);
+        graphdata->adjList.push_back(A);/* code */
+    } else cout << "ADD EDGE ERROR" << endl;
+}
+
+template <class T> 
+int Graph<T>::getVertices()
+{
+    return vertices;
+}
+
+template <class T>
+GraphData<T>* Graph<T>::getData(int pos)
+{
+    if (pos <= vertices && pos > 0 && head->get(pos) != nullptr){
+        return head->get(pos);
+    } else {
+        cout << "GETDATA ERROR" << endl;
+        return nullptr;
+    }
+}
+
+template <class T>
+void Graph<T>::printGraph()
+{
+    GraphData<T> *graphdata;
+    
+    for (size_t i = 0; i < vertices; i++)
+    {
+        graphdata = head->get(i+1);
+        cout << "NODO " << i+1 << endl;
+        cout << "Data: " << *(graphdata->data) << endl;
+        cout << "Cost: " << graphdata->cost << endl;
+        cout << "Linked to: ";
+        for (size_t i = 0; i < graphdata->adjList.size(); i++)
+        {
+            cout << graphdata->adjList[i] << ",";
+        }
+        cout << endl << endl;
+    }
+}
+
+template <class T>
+Graph<T>::~Graph(){  
+    for (size_t i = 0; i < vertices; i++)
+    {
+        delete getData(i+1);
+    }
+    delete head;
+}
+
+#endif
